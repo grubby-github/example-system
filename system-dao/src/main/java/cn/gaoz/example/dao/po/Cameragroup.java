7e95a39666e24e47307416c88b2eca21f403ba99
grubby-github <re_grubby@hotmail.com>
2019-11-05 11:45:41 +0800

    自动生成mapper
    自动生成mapper

--- system-dao/src/main/java/cn/gaoz/example/dao/po/Cameragroup.java
+++ system-dao/src/main/java/cn/gaoz/example/dao/po/Cameragroup.java
@@ -1,10 +1,7 @@
 package cn.gaoz.example.dao.po;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import java.util.Date;
+import javax.persistence.*;
 
 @Table(name = "`cameragroup`")
 public class Cameragroup {
